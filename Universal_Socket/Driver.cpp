9acbe4e3c5e669a7926c9ce1e553fec32e6d4ab3
Ethan <eceill@comcast.net>
2024-07-31 20:16:37 -0400

    Finished TCP Server and Client Starting functions

--- Universal_Socket/Driver.cpp
+++ Universal_Socket/Driver.cpp
@@ -1,6 +1,56 @@
 #include "Universal_Socket.h"
 
+#include <thread>
+
+void Server_Socket_Driver(Universal_Socket Server_Socket);
+
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// Entry point for Universal_Socket project
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 int main()
 {
-    printf("Hello World!\n");
-}
+   printf("This program will be the server!\n");
+
+   Socket_Vars::Protocol protocol_type = Socket_Vars::TCP;
+   Socket_Vars::Connection_Type connect_type = Socket_Vars::SERVER;
+   std::string ip_address = "127.0.0.1";
+   int port = 8080;
+
+   // Create the Server Socket object
+   Universal_Socket Server_Socket = Universal_Socket
+   (
+      protocol_type,
+      connect_type,
+      ip_address,
+      port
+   );
+
+   // Server thread to handle function loop for receiving and sending
+   std::thread Server_Thread(Server_Socket_Driver, Server_Socket);
+
+   Server_Thread.join();
+
+   return 0;
+}  // END main()
+
+void Server_Socket_Driver(Universal_Socket Server_Socket)
+{
+   bool keep_socket_alive = true;
+   unsigned char* buffer;
+
+   // Start the server socket
+   keep_socket_alive &= Server_Socket.Start();
+
+   // Main socket loop to receive and send messages to any client connections
+   while (keep_socket_alive)
+   {
+      
+   }
+
+   // Stop the server socket
+   if (!Server_Socket.Stop())
+   {
+      printf("Problem stopping the Server Socket!\n");
+   }
+
+}  // END Server_Socket_Driver()
--- Universal_Socket/Universal_Socket.cpp
+++ Universal_Socket/Universal_Socket.cpp
@@ -11,9 +11,12 @@
 // 07-05-2024
 //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
+// Need to link with Ws2_32.lib
+#pragma comment (lib, "Ws2_32.lib")
+
 #include "Universal_Socket.h"
 
-//#include <WS2tcpip.h>
+#include <WS2tcpip.h>
 
 //+-+-+-+-+-+-+-+-+-+-+PRIVATE FUNCTIONS+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
@@ -27,7 +30,7 @@ bool Universal_Socket::TCP_Server_Start()
    int opt_val = 0;
 
    //
-   // 1. Create the socket for TCP
+   // 1. Create the listen socket for TCP
    _listen_socket = socket(
       AF_INET,     // address family = IPV4
       SOCK_STREAM, // type = TCP
@@ -35,24 +38,27 @@ bool Universal_Socket::TCP_Server_Start()
    );
    if (SOCKET_ERROR == _listen_socket)
    {
-      printf("ERROR, socket() failed with: %u\n", WSAGetLastError());
+      printf("ERROR, Server socket() failed with: %u\n", WSAGetLastError());
       result &= false;
       return result;
    }
 
    //
-   // 2. Initialize the SOCKADDR_IN
-   memset(&_address, 0, sizeof(SOCKADDR_IN));
+   // 2. setsockopt for listen_socket with KEEPALIVE
+   setsockopt(_listen_socket, SOL_SOCKET, SO_KEEPALIVE, (char*)&opt_val, sizeof(int));
+
+   //
+   // 3. Initialize the SOCKADDR_IN
    _address.sin_family = AF_INET;
    _address.sin_addr.s_addr = inet_addr(_ip_address.c_str());
    _address.sin_port = htons(_port);
  
    //
-   // 3. Bind the socket to an IP address and port
+   // 4. Bind the socket to an IP address and port
    func_result = bind(_listen_socket, (SOCKADDR*)&_address, sizeof(_address));
    if (SOCKET_ERROR == func_result)
    {
-      printf("ERROR, bind() failed with: %u\n", WSAGetLastError());
+      printf("ERROR, Server bind() failed with: %u\n", WSAGetLastError());
       printf("Port = %d\n", _port);
       printf("IP Address = %s\n", _ip_address.c_str());
       result &= false;
@@ -60,11 +66,11 @@ bool Universal_Socket::TCP_Server_Start()
    }
 
    //
-   // 4. Listen for incoming connections
+   // 5. Listen for incoming connections
    func_result = listen(_listen_socket, 1);
    if (SOCKET_ERROR == func_result)
    {
-      printf("ERROR, listen() failed with: %u\n", WSAGetLastError());
+      printf("ERROR, Server listen() failed with: %u\n", WSAGetLastError());
       result &= false;
       return result;
    }
@@ -74,17 +80,17 @@ bool Universal_Socket::TCP_Server_Start()
    }
 
    //
-   // 5. Accept the incoming connection
+   // 6. Accept the incoming connection
    _socket = accept(_listen_socket, NULL, NULL);
    if (INVALID_SOCKET == _socket) 
    {
-      printf("ERROR, accept() failed with: %u\n", WSAGetLastError());
+      printf("ERROR, Server accept() failed with: %u\n", WSAGetLastError());
       result &= false;
       return result;
    }
 
    //
-   // 6. No longer need listen socket
+   // 7. No longer need listen socket
    closesocket(_listen_socket);
 
    return result;
@@ -95,8 +101,44 @@ bool Universal_Socket::TCP_Server_Start()
 //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 bool Universal_Socket::TCP_Client_Start()
 {
+   bool result = true;
+   int func_result = 0;
+   //
+   // 1. Attempt connection with the server
+   _socket = socket(
+      AF_INET,       // address family = IPV4
+      SOCK_STREAM,   // type = TCP
+      IPPROTO_TCP);  // protocol = TCP
+   if (INVALID_SOCKET == _socket)
+   {
+      printf("ERROR, Client socket() failed with: %u\n", WSAGetLastError());
+      result &= false;
+      return result;
+   }
 
-}
+   //
+   // 2. Initialize the SOCKADDR_IN
+   _address.sin_family = AF_INET;
+   _address.sin_addr.s_addr = inet_addr(_ip_address.c_str());
+   _address.sin_port = htons(_port);
+
+   //
+   // 3. Make a connection with the server
+   func_result = connect(_socket, (SOCKADDR*) &_address, sizeof(_address));
+   if (0 != func_result)
+   {
+      printf("ERROR, Client connect() failed with: %u\n", WSAGetLastError());
+      result &= false;
+      return result;
+   }
+   else
+   {
+      printf("Client: Ready for sending and/or receiving messages...\n");
+      result &= true;
+   }
+
+   return result;
+}  // END TCP_Client_Start()
 
 //-+-+-+-+-+-+-+-+-+-+-+PUBLIC FUNCTIONS+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
@@ -118,6 +160,7 @@ Universal_Socket::Universal_Socket
    _port = port;
    _socket = INVALID_SOCKET;
    _listen_socket = INVALID_SOCKET;
+   memset(&_address, 0, sizeof(SOCKADDR_IN));
 
    // function level variables
    int func_result = 0;
@@ -183,6 +226,18 @@ bool Universal_Socket::Start()
 
    return result;
 }  // END Start()
+
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// Opens the socket ready to receive and or send
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+bool Receive(unsigned char* buffer)
+{
+   bool result = true;
+
+
+
+   return result;
+}
 
 //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+    
 /// Closes the socket
